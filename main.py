66a9561295aea21744aae3e0a3928d6143ad5640
Joel Cespedes <joelguitar@gmail.com>
2026-01-12 17:14:08 +0100

    Update CORS for multiple ports

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def create_default_admin():
 FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:3000")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[FRONTEND_URL, "http://localhost:3000"],
+    allow_origins=[FRONTEND_URL, "http://localhost:3000", "http://localhost:3001", "http://localhost:3002"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
